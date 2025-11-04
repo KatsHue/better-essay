138f9137d8b55187aa45d69006d1538d4c508035
KatsHue <katia.shuerta@gmail.com>
2025-11-03 19:04:51 -0600

    correo de better-essay agregado a sendconfirmation

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whitelist = [
-      process.env.FRONTEND_URL,
+      //process.env.FRONTEND_URL,
       "http://localhost:5173", // desarrollo local
       "https://better-essay.onrender.com",
     ];
